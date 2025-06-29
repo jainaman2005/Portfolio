a7d28ebf713d08a8baf62e533c4be98b1af78aff
Aman jain <jainaman102020@gmail.com>
2025-06-29 20:29:03 +0530

    v1 final

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Header from './components/Header/header.jsx'
-// import Footer from './components/Footer/footer.jsx'
+import Footer from './components/Footer/footer.jsx'
 import Home from './components/Home/home.jsx'
 import About from './components/About/about.jsx'
 import Projects from './components/Projects/project.jsx'
@@ -17,7 +17,7 @@ function App() {
       <Projects/>
       <Experience/>
       <Contact/>
-      {/* <Footer/> */}
+      <Footer/>
     </>
   )
 }
